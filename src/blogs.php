204582b9c3763ac23ef97d82d369a3b86a2eda0a
odarym <mosesodary101@gmail.com>
2024-10-17 19:44:43 +0300

    Initial project outline

---
+++ src/blogs.php
@@ -0,0 +1,125 @@
+<?php
+	include "includes/header.php"
+?>
+	<div class="mb-5 mt-5 d-flex">
+		<div class="card-group">
+		  <div class="card">
+		    <img src="assets/images/e46-m3-csl-profile-3.jpg" class="card-img-top" alt="Image">
+		    	<div class="card-body">
+		    		<h5 class="card-title">Card title</h5>
+		    		<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		    	</div>
+		    	<div class="card-footer">
+		      		<small class="text-body-secondary">Last updated 3 mins ago</small>
+				</div>
+		  </div>
+			<div class="card">
+				<img src="assets/images/e46-m3-csl-profile-1.jpg" class="card-img-top" alt="Image">
+				<div class="card-body">
+		    		<h5 class="card-title">Card title</h5>
+		    		<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+				</div>
+		    <div class="card-footer">
+		      <small class="text-body-secondary">Last updated 3 mins ago</small>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <img src="assets/images/e46-m3-csl-profile.jpg" class="card-img-top" alt="Image">
+		    <div class="card-body">
+		      <h5 class="card-title">Card title</h5>
+		      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+		    </div>
+		    <div class="card-footer">
+		      <small class="text-body-secondary">Last updated 3 mins ago</small>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<!-- Blog Cards -->
+	<div class="mb-5 mt-5 d-flex">
+		<div class="card-group">
+  			<div class="card">
+  				<img src="assets/images/e46-m3-csl-profile-1.jpg" class="card-img-top" alt="Image">
+  				<div class="card-body">
+  			    	<h5 class="card-title">Card title</h5>
+  			    	<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  			    	<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  				</div>
+  			</div>
+  			<div class="card">
+  			  	<img src="assets/images/e46-m3-csl-profile-3.jpg" class="card-img-top" alt="Image">
+  				<div class="card-body">
+  			  		<h5 class="card-title">Card title</h5>
+  			    	<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+  			    	<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  				</div>
+  			</div>
+  			<div class="card">
+  				<img src="assets/images/e46-m3-csl-profile.jpg" class="card-img-top" alt="Image">
+  				<div class="card-body">
+  			    	<h5 class="card-title">Card title</h5>
+  			    	<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+  			    	<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  				</div>
+  			</div>
+		</div>
+	</div>
+	<!-- Blog Cards -->
+
+	<!-- Blog card 3 -->
+	<div class="container mb-5 mt-5">
+		<section class="d-flex">
+			<main>
+				<div class="card" style="width: 90%;">
+  					<img src="assets/images/e46-m3-csl.jpg" class="card-img-top" alt="Image"></img>
+  					<div class="card-body">
+  						<h5 class="card-title">The legendary E46 M3 CSL</h5>
+  						<p class="card-text">The e46 M3 CSL is my favourite BMW of all time...</p>
+					</div>
+					<div class="card-body">
+						<a href="#" class="btn btn-primary">Read More</a>
+					</div>
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item">
+							<p>Article by Odary</p>
+							<small class="text-body-secondary">Last updated a long time ago</small>
+						</li>
+  					</ul>
+				</div>
+			</main>
+			<aside></aside>
+		</section>
+	</div>
+	<!-- END Blog Card 3 -->
+
+	<!-- Blog card 4 -->
+	<div class="container mb-5 mt-5">
+		<section class="d-flex">
+			<main>
+				<div class="card" style="width: 90%;">
+  					<img src="assets/images/fall-sunrise.jpg" class="card-img-top" alt="Image"></img>
+  					<div class="card-body">
+  						<h5 class="card-title">Stunning fall sunrises</h5>
+  						<p class="card-text">We all love great views whether it's winter time snow...</p>
+					</div>
+					<div class="card-body">
+						<a href="#" class="btn btn-primary">Read More</a>
+					</div>
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item">
+							<p>Article by Kawira</p>
+							<small class="text-body-secondary">Last updated a long time ago</small>
+						</li>
+  					</ul>
+				</div>
+			</main>
+		</section>
+	</div>
+	<!-- END Blog Card 4 -->
+
+
+
+<?php
+	include "includes/footer.php"
+?>
